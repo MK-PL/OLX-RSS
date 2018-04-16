e2365abcb2dc65b0596960428ce252474d6be968
Maciej Kawa <mk-pl@mailplus.pl>
2018-04-16 20:13:58 +0200

    Wersja 0.1.0

---
+++ README.md
@@ -0,0 +1,46 @@
+# OLX RSS
+OLX RSS jest generatorem kanału RSS do serwisu OLX.pl, który pozwala na spersonalizowanie wszystkich parametrów wyszukiwania.
+
+## Jak zainstalować?
+
+Skopiuj pliki na serwer obsługujący PHP w wersji >= 5.3 i otwórz w przeglądarce adres http://ADRES_SERWERA/index.html
+
+## Jak stworzyć kanał RSS?
+-
+1. Wejdź w wybraną przez Ciebie kategorię na stronie OLX.pl i ustaw parametry wyszukiwania.<br/>Ustaw sortowanie ogłoszeń po najnowszych, żeby śledzić najnowsze ogłoszenia w kanale RSS. W kanale RSS domyślnie wyświetlane są ogłoszenia z dwóch pierwszych podstron z wynikami. 
+
+![alt tag](https://raw.githubusercontent.com/MK-PL/OLXRSS/master/img/img1.png)
+
+2. Przekopiuj link URL z pola adresu WWW przeglądarki. Powinny być w nim zamieszczone parametry wyszukiwania.
+
+![alt tag](https://raw.githubusercontent.com/MK-PL/OLXRSS/master/img/img2.png)
+
+3. Wprowadź adres do wskazanego pola na stronie internetowej skryptu i naciśnij przycisk "Generuj".
+
+![alt tag](https://raw.githubusercontent.com/MK-PL/OLXRSS/master/img/img3.png)
+
+4. Zostanie wygenerowany kanał RSS z ogłoszeniami o podanych parametrach wyszukiwaniach, jak w podanym adresie URL do serwisu OLX.pl.
+
+![alt tag](https://raw.githubusercontent.com/MK-PL/OLXRSS/master/img/img4.png)
+
+## Co zostało użyte do zrobienia OLX RSS?
+-
+- HTML/CSS/PHP
+- FeedWriter (https://github.com/mibe/FeedWriter)
+- PHP Simple HTML DOM Parser (http://simplehtmldom.sourceforge.net)
+
+## Zmiany
+
+- 0.1.0 Start skryptu.
+
+## Błędy
+
+Wykryte błędy proszę zgłaszać w sekcji 'Issues' projektu - dzięki temu inni użytkownicy będą mogli zapoznać się z problemem (zwłaszcza, jeśli mają podobny) i ewentualnie zaproponować swoje rozwiązania.
+
+## Autor
+
+Maciej Kawa
+
+Chcesz wesprzeć moją pracę? Możesz pozostawić symboliczną dotację https://www.paypal.me/MaciejKawa
+
+kontakt [at] maciejkawa.lubin.pl
---
+++ src/get.php
@@ -0,0 +1,82 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+
+	include 'simple_html_dom.php';
+	include 'Item.php';
+	include 'Feed.php';
+	include 'RSS2.php';
+  
+  date_default_timezone_set('UTC');
+  setlocale(LC_ALL, array( "pl_PL", "polish_pol" ));
+	use \FeedWriter\RSS2;
+  
+	$TestFeed = new RSS2;
+	$TestFeed->setTitle($_GET['url']);
+  
+  $baseUrl = html_entity_decode($_GET['url']);
+  $baseUrl = str_replace("&view=galleryWide", "", $baseUrl);
+  $baseUrl = str_replace("?view=galleryWide", "", $baseUrl);
+  $baseUrl = trim($baseUrl, '/');
+  
+  $context = stream_context_create(array('http' => array('header' => 'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0')));
+  
+  $html = file_get_html($baseUrl, false, $context, 0);
+  
+  if(($html->find('.pager', 0))) {
+    $numberOfPages = 2;
+  } else {
+    $numberOfPages = 1;
+  }
+
+  $j = 0;
+
+  for ($i = 1; $i <= $numberOfPages; $i++) {
+    if (strpos($baseUrl, '?')) {
+      $page = file_get_html($baseUrl.'&page='.$i, false, $context, 0);
+    } else {
+      $page = file_get_html($baseUrl.'/?page='.$i, false, $context, 0);
+    }
+    
+    
+    foreach($page->find('#offers_table .wrap') as $article) {
+
+        $item[$j] = $TestFeed->createNewItem();
+        $item[$j]->setTitle($article->find('.detailsLink strong', 0)->plaintext);
+        $item[$j]->setLink($article->find('.detailsLink', 0)->href);
+        
+        $price = $article->find('.price', 0)->plaintext;
+        $location = $article->find('.marginbott5', 0)->plaintext;
+        $date = $article->find('p.x-normal', 0)->plaintext;
+        if (strpos($date, 'dzisiaj')) {
+          $date = strftime('%e %b');
+        } elseif (strpos($date, 'wczoraj')) {
+          $date = strftime('%e %b', strtotime('-1 days'));
+        }
+      
+        $item[$j]->setDescription ('
+        <table cellpadding="10">
+          <tbody>
+            <tr>
+              <td width="300" bgcolor="#eeeeee">
+                <a href="'.$article->find('.detailsLink', 0)->href.'" target="_blank">
+                  <center>
+                    <img src="'.$article->find('.linkWithHash img', 0)->attr['src'].'">
+                  </center>
+                </a>
+              </td>
+              <td>
+                <p>Miejscowość: <strong>'.trim($location).',</strong> Wystawiono: '.$date.'</p><h1>Cena: '.$price.'</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table><hr>');
+      
+        $TestFeed->addItem($item[$j]);
+        $j++;
+    }
+  }
+
+	$TestFeed->printFeed();
+?>
